cb03d07a828f05f0d3ea9efbef984c299673d366
IlliumIv <alm.sparta.ao@gmail.com>
2022-03-09 17:20:57 +0500

    Change event trigger to prevent cannons deactivate for whole crew

--- Scripts/Cannoneer'sParrot.lua
+++ Scripts/Cannoneer'sParrot.lua
@@ -1,19 +1,20 @@
-function Shot()
-	local deviceId = avatar.GetActiveUsableDevice()
-	local deviceInfo = avatar.GetUsableDeviceInfo(deviceId)
-	if deviceInfo then
-		local deviceName = userMods.FromWString(deviceInfo.name)
-		deviceName = string.lower(deviceName)
-		local isCannon = string.find(deviceName, "cannon") or string.find(deviceName, "пушка")
-		if isCannon then
-			avatar.DeactivateUsableDevice()
+function CooldownStarted(device)
+	local usableDeviceId = avatar.GetActiveUsableDevice()
+	if usableDeviceId ~= nil and device.id == usableDeviceId then
+		local deviceInfo = avatar.GetUsableDeviceInfo(usableDeviceId)
+		if deviceInfo then
+			local deviceName = string.lower(userMods.FromWString(deviceInfo.name))
+			local isCannon = string.find(deviceName, "cannon") or string.find(deviceName, "пушка")
+			if isCannon then
+				avatar.DeactivateUsableDevice()
+			end
 		end
 	end
 end
 
 function Init()
 	common.UnRegisterEventHandler(Init, "EVENT_AVATAR_CREATED")
-	common.RegisterEventHandler(Shot, "EVENT_CANNON_SHOT_STARTED")
+	common.RegisterEventHandler(CooldownStarted, "EVENT_DEVICE_COOLDOWN_STARTED")
 end
 
 common.RegisterEventHandler(Init, "EVENT_AVATAR_CREATED")
