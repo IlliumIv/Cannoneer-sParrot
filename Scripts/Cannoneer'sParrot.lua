3d9342d70a3ce92bfe850ef7ee68cb365af9fd0b
IlliumIv <alm.sparta.ao@gmail.com>
2022-03-09 14:18:47 +0500

    Add device name check to prevent devices deactivate for whole crew

--- Scripts/Cannoneer'sParrot.lua
+++ Scripts/Cannoneer'sParrot.lua
@@ -1,5 +1,14 @@
 function Shot()
-	avatar.DeactivateUsableDevice()
+	local deviceId = avatar.GetActiveUsableDevice()
+	local deviceInfo = avatar.GetUsableDeviceInfo(deviceId)
+	if deviceInfo then
+		local deviceName = userMods.FromWString(deviceInfo.name)
+		deviceName = string.lower(deviceName)
+		local isCannon = string.find(deviceName, "cannon") or string.find(deviceName, "пушка")
+		if isCannon then
+			avatar.DeactivateUsableDevice()
+		end
+	end
 end
 
 function Init()
