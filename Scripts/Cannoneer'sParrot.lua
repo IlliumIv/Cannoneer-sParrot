e7936b3579cbecbbcdde9dc8b3205e5fc131ed3e
IlliumIv <alm.sparta.ao@gmail.com>
2022-03-09 13:47:23 +0500

    Init

---
+++ Scripts/Cannoneer'sParrot.lua
@@ -0,0 +1,11 @@
+function Shot()
+	avatar.DeactivateUsableDevice()
+end
+
+function Init()
+    common.UnRegisterEventHandler(Init, "EVENT_AVATAR_CREATED")
+	common.RegisterEventHandler(Shot, "EVENT_CANNON_SHOT_STARTED")
+end
+
+common.RegisterEventHandler(Init, "EVENT_AVATAR_CREATED")
+if avatar.IsExist() then Init() end
